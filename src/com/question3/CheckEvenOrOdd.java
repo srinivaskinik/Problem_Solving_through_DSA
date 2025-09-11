2d8d4568f96702d4a43adb7561335f30e6ab0d7a
srinivaskinik <srinivaskinik@gmail.com>
2025-09-11 14:58:21 +0530

    check number is even or odd

---
+++ src/com/question3/CheckEvenOrOdd.java
@@ -0,0 +1,14 @@
+package com.question3;
+
+public class CheckEvenOrOdd {
+	
+	public static void main(String[] args) {
+		int number =5;
+		if((number & 1) ==1) {
+			System.out.println("ODD NUMBER");
+		} else {
+			System.out.println("EVEN NUMBER");
+		}
+	}
+
+}
