d3cc9fa6adb745ebd797021eeeb22118aba5a7b6
srinivaskinik <srinivaskinik@gmail.com>
2025-09-12 10:52:48 +0530

    factorial of a number

---
+++ src/com/question4/FactorialOfALargeNumber.java
@@ -0,0 +1,19 @@
+package com.question4;
+
+import java.math.BigInteger;
+
+public class FactorialOfALargeNumber {
+
+	public static void main(String[] args) {
+		System.out.println(factorial(50));
+	}
+
+	public static BigInteger factorial(int num) {
+		BigInteger result = BigInteger.ONE;
+		for (int i = 1; i <= num; i++) {
+			result = result.multiply(BigInteger.valueOf(i));
+		}
+		return result;
+	}
+
+}
