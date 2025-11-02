3565a436eafcdcb7e269ff8ac388ed150d1b727c
srinivaskinik <srinivaskinik@gmail.com>
2025-11-02 05:59:34 +0530

    String manipulation

---
+++ src/com/logicBuildingExercises/StringManipulation.java
@@ -0,0 +1,24 @@
+package com.logicBuildingExercises;
+
+public class StringManipulation {
+
+	public static void main(String[] args) {
+		String input = "99956";
+		// If numbers are odd and repeating then should put -. So output will be 9-9-956
+
+		StringBuilder result = new StringBuilder();
+		for (int i = 0; i < input.length() - 1; i++) {
+			result.append(input.charAt(i)); // append current character
+			int currentNumber = Character.getNumericValue(input.charAt(i));
+			int nextNumber = Character.getNumericValue(input.charAt(i + 1));
+
+			if (currentNumber % 2 != 0 && nextNumber % 2 != 0 && currentNumber == nextNumber) {
+				result.append("-");
+			}
+		}
+		result.append(input.charAt(input.length() - 1));
+		System.out.println(result.toString());
+
+	}
+
+}
